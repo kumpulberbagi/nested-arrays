e6a7f9dbd2668a78e205016334e029bc6eabd1f2
tonychen09 <tony_chen93@yahoo.com>
2016-09-28 20:43:29 +0700

    nested-array

--- chessboard.js
+++ chessboard.js
@@ -1,4 +1,3 @@
-//write your code here
 var chessboard = [['Benteng Hitam','Kuda Hitam','Menteri Hitam','Raja Hitam','Ratu Hitam','Menteri Hitam','Kuda Hitam','Benteng Hitam'],
                   ['Pion Hitam','Pion Hitam','Pion Hitam','Pion Hitam','Pion Hitam','Pion Hitam','Pion Hitam','Pion Hitam'],
                   ['','','','','','','',''],
@@ -8,4 +7,21 @@ var chessboard = [['Benteng Hitam','Kuda Hitam','Menteri Hitam','Raja Hitam','Ra
                   ['Pion Putih','Pion Putih','Pion Putih','Pion Putih','Pion Putih','Pion Putih','Pion Putih'],
                   ['Benteng Putih','Kuda Putih','Menteri Putih','Raja Putih','Ratu Putih','Menteri Putih','Kuda Putih','Benteng Putih']]
 
+var tampungChess = []
+for (var i=0;i<8;i++)
+{
+  if(i==0){
+  tampungChess.push(['Benteng Hitam','Kuda Hitam','Menteri Hitam','Raja Hitam','Ratu Hitam','Menteri Hitam','Kuda Hitam','Benteng Hitam'])
+}else if(i==1)
+tampungChess.push(['Pion Hitam','Pion Hitam','Pion Hitam','Pion Hitam','Pion Hitam','Pion Hitam','Pion Hitam','Pion Hitam'])
+else if(i==2 || i==3 || i==4 || i==5){
+  tampungChess.push(['','','','','','','',''])
+}else if(i==6)
+{
+  tampungChess.push(['Pion Putih','Pion Putih','Pion Putih','Pion Putih','Pion Putih','Pion Putih','Pion Putih'])
+}else {
+    tampungChess.push(['Benteng Putih','Kuda Putih','Menteri Putih','Raja Putih','Ratu Putih','Menteri Putih','Kuda Putih','Benteng Putih'])
+}
+}
+console.log(tampungChess);
 console.log(chessboard[7][0]);
--- spreadsheet.js
+++ spreadsheet.js
@@ -5,5 +5,26 @@ var spreadsheet = [['Number','Name','Position','Points per Game'],
                   [31,'Harvey Kay','Shooting Guard',[0,30,16,0,25]],
                   [18,'Sally Talls','Power Forward',[18,29,26,31,19]],
                   [22,'Mo DiBoux','Small Forward',[11,0,23,17,0]]]
+
+
+var tampungSpreadSheet = []
+for (var i=0;i<6;i++)
+{
+  if(i==0){
+  tampungSpreadSheet.push(['Number','Name','Position','Points per Game'])
+}else if(i==1)
+tampungSpreadSheet.push([12,'Joe Schmo','Center',[14,32,7,0,23]])
+else if(i==2){
+  tampungSpreadSheet.push([9,'Ken Buckets','Point Guard',[19,0,11,22,0]])
+}else if(i==3)
+{
+  tampungSpreadSheet.push([31,'Harvey Kay','Shooting Guard',[0,30,16,0,25]])
+}else if(i==4){
+    tampungSpreadSheet.push([18,'Sally Talls','Power Forward',[18,29,26,31,19]])
+}else {
+    tampungSpreadSheet.push([22,'Mo DiBoux','Small Forward',[11,0,23,17,0]])
+}
+}
+console.log(tampungSpreadSheet);
 console.log(spreadsheet[3][2] === "Shooting Guard");//true
 console.log(JSON.stringify(spreadsheet[1][3]) === JSON.stringify([14,32,7,0,23]));//true
