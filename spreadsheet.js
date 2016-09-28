5ebf389d34a85a5307b752da0cff5e323c35213e
broerjuang <juangwiantoro@gmail.com>
2016-09-28 12:12:29 +0700

    release 1 update

--- spreadsheet.js
+++ spreadsheet.js
@@ -1,17 +1,18 @@
 //Set all global variable
 const title = ["Number", "Name", "Postition", "Points Per Game"];
-const user_1 = [1, "Alfa", "Back", [0,12,3,44,23]];
-const user_2 = [2, "Beta", "Centeria", [14,32,7,0,23]];
-const user_3 = [3, "Charlie", "Front", [5,23,7,0,23]];
-const user_4 = [3, "Delta", "Back", [7,32,7,12,23]];
-const user_5 = [4, "Etsy", "Center", [1,32,35,0,23]];
-const all_user = [user_1, user_2, user_3, user_4, user_5];
+var all_user = [title];
+
+// function that store the data from user
+function data_table(array) {
+  all_user.push(array);
+  return formated_data();
+}
 
 // Make a function that checking all data and compile it into table
 function formated_data() {
   var table = [];
   for (var i = 0; i <= title.length; i += 1) {
-    table[i] = [];
+    table = [];
     for (var j = 1; j < all_user.length; j += 1) {
       table[0] = title;
       table[j] = all_user[j];
@@ -21,6 +22,7 @@ function formated_data() {
 }
 
 
-// testing
-var member = formated_data();
-console.log(member[1][2]);
+// Testing should store all the data and the input is array
+data_table([3, "Charlie", "Front", [5,23,7,0,23]];
+data_table([1, "Alfa", "Back", [0,12,3,44,23]];
+console.log(data_table([4, "Etsy", "Center", [1,32,35,0,23]]));
