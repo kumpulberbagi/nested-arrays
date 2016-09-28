6475795eb5e78c946ee6b7bd0ada07c2423fcdf1
Septian A Tama <tama@tamatamvan.web.id>
2016-09-28 11:34:00 +0700

    displaying nested array
    displaying nested array release 0 (chessboard), & release 1
    (spreadsheet). Sementara masih pakai manual.

--- chessboard.js
+++ chessboard.js
@@ -1,1 +1,27 @@
 //write your code here
+var chessboard = [
+  [ ["black rock"],["black knight"],["black bishop"],["black king"],["black queen"],["black bishop"],["black knight"],["black rock"] ],
+  [ ["black pawn"],["black pawn"],["black pawn"],["black pawn"],["black pawn"],["black pawn"],["black pawn"],["black pawn"] ],
+  [ [],[],[],[],[],[],[],[] ],
+  [ [],[],[],[],[],[],[],[] ],
+  [ [],[],[],[],[],[],[],[] ],
+  [ [],[],[],[],[],[],[],[] ],
+  [ ["white pawn"],["white pawn"],["white pawn"],["white pawn"],["white pawn"],["white pawn"],["white pawn"],["white pawn"] ],
+  [ ["white rock"],["white knight"],["white bishop"],["white queen"],["white king"],["white bishop"],["white knight"],["white rock"] ]
+];
+
+var displayChess = () => {
+  console.log("===========CHESSBOARD===========");
+  for (var i = 0; i < (chessboard.length); i++) {
+    var chess = "";
+    for (var j = 0; j < chessboard[i].length; j++) {
+      chess += "| " + chessboard[i][j] + " |";
+    }
+    console.log(chess);
+  }
+  return "===========END===========";
+}
+
+console.log(displayChess());
+
+console.log();
--- spreadsheet.js
+++ spreadsheet.js
@@ -1,1 +1,36 @@
 //write your code here
+var basketball = [
+  ["Number", "Name", "Position", "Poinst per Game"],
+  ["12", "Joe Schmo", "Center", [14, 32, 7, 0, 23]],
+  ["9", "Ken Buckets", "Point Guard", [19, 0, 11, 22, 0]],
+  ["31", "Harvey Kay", "Shooting Guard", [0, 30, 16, 0, 25]],
+  ["18", "Sally Talls", "Power Forward", [18, 29, 26, 31, 19]],
+  ["22", "Mo Diboux", "Small Forward", [11,0, 23, 17,0]]
+];
+
+function displayHeader() {
+  console.log("===========START===========");
+  var header = "";
+  for (var i = 0; i < basketball[0].length; i++) {
+    header += "| " + basketball[0][i] + " |";
+  }
+  return header;
+}
+
+var displayPlayer = () => {
+  for (var i = 1; i < (basketball.length); i++) {
+    var player = "";
+    for (var j = 0; j < basketball[i].length; j++) {
+      player += "| " + basketball[i][j] + " |";
+    }
+    console.log(player);
+  }
+  return "===========END===========";
+}
+
+console.log(displayHeader());
+console.log(displayPlayer());
+
+console.log("===========TESTING===========");
+console.log(basketball[3][2] === "Shooting Guard"); //true
+console.log(JSON.stringify(basketball[1][3] )== JSON.stringify([14, 32, 7, 0, 23])); //true
