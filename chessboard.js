33d2bfde418a69d5440abb2477776b10d2fe3e8e
Izhharuddin Syanmil <email": null>
2016-09-28 10:35:44 +0700

    release 0

--- chessboard.js
+++ chessboard.js
@@ -1,1 +1,27 @@
-//write your code here
+// //write your code here
+var board = [["benteng hitam", "kuda hitam", "gajah hitam", "ratu hitam", "raja hitam", "gajah hitam", "kuda hitam", "benteng hitam"],
+              ["pion hitam","pion hitam","pion hitam","pion hitam","pion hitam","pion hitam","pion hitam","pion hitam","pion hitam"],
+              ["","","","","","","",""],
+              ["","","","","","","",""],
+              ["","","","","","","",""],
+              ["","","","","","","",""],
+              ["pion putih", "pion putih","pion putih","pion putih","pion putih","pion putih","pion putih","pion putih","pion putih"],
+              ["benteng putih", "kuda putih", "gajah putih", "ratu putih", "raja putih", "gajah putih", "kuda putih", "benteng putih"]
+            ]
+console.log(board[7][0])
+var satuduatiga = [];
+// var fill = (arr) => {
+//   for (var i = 0; i<8; i++){
+//   arr.push(i)
+//   }
+// }
+// fill(satuduatiga);
+// var pion = (n, board, warna) => {
+//   var arr = board[n];
+//   for (var j = 0; j<8; j++){
+//   arr.push("pion " + warna);
+//   }
+// }
+// pion(1, satuduatiga, "putih");
+// pion(6, satuduatiga, "hitam");
+// console.log(satuduatiga);
