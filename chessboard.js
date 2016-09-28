64db27733d630df7dde433ac51912df2828298d9
tonychen09 <tony_chen93@yahoo.com>
2016-09-28 10:51:16 +0700

    'nested-array'

--- chessboard.js
+++ chessboard.js
@@ -1,1 +1,11 @@
 //write your code here
+var chessboard = [['Benteng Hitam','Kuda Hitam','Menteri Hitam','Raja Hitam','Ratu Hitam','Menteri Hitam','Kuda Hitam','Benteng Hitam'],
+                  ['Pion Hitam','Pion Hitam','Pion Hitam','Pion Hitam','Pion Hitam','Pion Hitam','Pion Hitam','Pion Hitam'],
+                  ['','','','','','','',''],
+                  ['','','','','','','',''],
+                  ['','','','','','','',''],
+                  ['','','','','','','',''],
+                  ['Pion Putih','Pion Putih','Pion Putih','Pion Putih','Pion Putih','Pion Putih','Pion Putih'],
+                  ['Benteng Putih','Kuda Putih','Menteri Putih','Raja Putih','Ratu Putih','Menteri Putih','Kuda Putih','Benteng Putih']]
+
+console.log(chessboard[7][0]);
