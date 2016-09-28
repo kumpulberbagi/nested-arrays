74ada7db6d8417fff1e276e17175985bf46e3c99
Alexander Hendrawan tjahja <email": null>
2016-09-28 11:22:23 +0700

    tugas Nested array

--- chessboard.js
+++ chessboard.js
@@ -1,1 +1,12 @@
-//write your code here
+var chess = [
+  ["Benteng Hitam", "Kuda Hitam", "Menteri Hitam", "Raja Hitam", "Ratu Hitam", "Menteri Hitam", "Kuda Hitam", "Benteng Hitam"],
+  ["Pion Hitam", "Pion Hitam", "Pion Hitam", "Pion Hitam", "Pion Hitam", "Pion Hitam", "Pion Hitam", "Pion Hitam"],
+  ["", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", ""],
+  ["Pion Putih", "Pion Putih", "Pion Putih", "Pion Putih", "Pion Putih", "Pion Putih", "Pion Putih", "Pion Putih"],
+  ["Benteng Putih", "Kuda Putih", "Menteri Putih", "Raja Putih", "Ratu Putih", "Menteri Putih", "Kuda Putih", "Benteng Putih"]
+]
+
+console.log(chess[7][0] == "Benteng Putih"); // true
--- spreadsheet.js
+++ spreadsheet.js
@@ -1,1 +1,14 @@
 //write your code here
+var table = [
+  ["Number", "Name", "Position", "Points per Game"],
+  [12, "Joe Schmo", "Center", [14, 32, 7, 0, 23]],
+  [9, "Ken Buckets", "Point Guard", [19, 0, 11, 22, 0]],
+  [31, "Harvey Kay", "Shooting Guard", [0, 30, 16, 0, 25]],
+  [18, "Sallt Talls", "Power Forward", [18, 29, 26, 31, 19]],
+  [22, "Mo Diboux", "Small Forward", [11, 0, 23, 17, 0]]
+]
+
+console.log(JSON.stringify(table[1][3]) == JSON.stringify([14, 32, 7, 0, 23]));
+//console.log(table[1][3] == [14, 32, 7, 0, 23]); // false, why?
+//console.log(JSON.stringify(table[1][3]));
+//console.log(JSON.stringify([14, 32, 7, 0, 23]));
