5aeacfc108277c0d4176751e26137dac705f6058
mangkuwi26 <mangkuwi26@gmail.com>
2016-09-28 15:01:13 +0700

    submit tugas

--- chessboard.js
+++ chessboard.js
@@ -1,1 +1,12 @@
-//write your code here
+	// //write your code here
+	var chessboard = [
+		["BentengHitam","KudaHitam","GajahHitam","RajaHitam", "MentriHitam","GajahHitam","KudaHitam","BentengHitam"],
+		["PionHitam","PionHitam","PionHitam","PionHitam","PionHitam","PionHitam","PionHitam"],
+		[[],[],[],[],[],[],[]],
+		[[],[],[],[],[],[],[]],
+		[[],[],[],[],[],[],[]],
+		[[],[],[],[],[],[],[]],
+		["PionPutih","PionPutih","PionPutih","PionPutih","PionPutih","PionPutih","PionPutih",],
+		["BentengPutih","KudaPutih","GajahPutih", "RajaPutih","MentriPutih","GajahPutih","KudaPutih","BentengPutih"]
+	];
+	 console.log(chessboard[7][0] == "BentengPutih");
--- spreadsheet.js
+++ spreadsheet.js
@@ -1,1 +1,13 @@
-//write your code here
+ //write your code here
+var data1 = [
+		["Number","Name","Position","Point per Game",""],
+		[12,"Joe Schmo","Center",[14,32,7,0,23]],
+		[9,"Ken Buckets","Point Guard",[19,0,11,22,0]],
+		[31,"Harvey Kay","Shooting Guard",[0,30,16,0,25]],
+		[18,"Sally Talls","Power Forward",[18,29,26,31,19]],
+		[22,"Mo DiBoux","Small Forward",[11,0,23,17,0]]
+];
+
+
+// console.log(data1[3][2] == "Shooting Guard");
+// console.log(JSON.stringify(table[1][3])== JSON.stringify([14,32,7,0,23]));
