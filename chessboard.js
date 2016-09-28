bb239b767ed63832c5373a45cb86cb07a0496a1e
dharmadi93 <dharmadi93@gmail.com>
2016-09-28 14:12:36 +0700

    nester arrays

--- chessboard.js
+++ chessboard.js
@@ -1,1 +1,85 @@
 //write your code here
+
+
+
+
+// var chessBoard = [
+//     ["Benteng Hitam", "Kuda Hitam", "Kuncum Hitam", "Ratu Hitam", "Raja Hitam", "Kuncum Hitam", "Kuda Hitam", "Benteng Hitam"],
+//     ["Pion Hitam", "Pion Hitam", "Pion Hitam", "Pion Hitam", "Pion Hitam", "Pion Hitam", "Pion Hitam", "Pion Hitam"],
+//     [],
+//     [],
+//     [],
+//     [],
+//     ["Pion Putih", "Pion Putih", "Pion Putih", "Pion Putih", "Pion Putih", "Pion Putih", "Pion Putih", "Pion Putih"],
+//     ["Benteng Putih", "Kuda Putih", "Kuncum Putih", "Ratu Putih", "Raja Putih", "Kuncum Putih", "Kuda Putih", "Benteng Putih"]
+// ];
+
+var x = new Array(8);
+
+for(var i=0;i<8;i++) {
+    x[i] = [];
+    for(var j=0; j<8;j++) {
+
+        x[i][j] = [];
+
+        if(i == 1) {
+            x[i][j].push("Pion Hitam");
+        }
+
+        if(i == 6) {
+            x[i][j].push("Pion Putih");
+        }
+
+        if((i == 0 && j == 0) || i ==0 && j ==7) {
+            x[i][j].push("Benteng Hitam");
+        }
+
+        if((i == 0 && j == 1) || i ==0 && j ==6) {
+            x[i][j].push("Kuda Hitam");
+        }
+
+        if((i == 0 && j == 2) || i ==0 && j ==5) {
+            x[i][j].push("Kuncum Hitam");
+        }
+
+        if((i == 0 && j == 3)) {
+            x[i][j].push("Ratu Hitam");
+        }
+        if((i == 0 && j == 4)) {
+            x[i][j].push("Raja Hitam");
+        }
+
+
+
+
+
+        if((i == 7 && j == 0) || i ==7 && j ==7) {
+            x[i][j].push("Benteng Putih");
+        }
+
+        if((i == 7 && j == 1) || i ==7 && j ==6) {
+            x[i][j].push("Kuda Putih");
+        }
+
+        if((i == 7 && j == 2) || i ==7 && j ==5) {
+            x[i][j].push("Kuncum Putih");
+        }
+
+        if((i == 7 && j == 3)) {
+            x[i][j].push("Ratu Putih");
+        }
+        if((i == 7 && j == 4)) {
+            x[i][j].push("Raja Putih");
+        }
+
+        // if(i == 1) {
+        //     x[i].push("Pion Putih");
+        // }
+    }
+
+
+}
+
+// console.log(x);
+// console.log(chessBoard[7][0] == "Benteng Putih");
+console.log(x[7][0] == "Benteng Putih");
--- spreadsheet.js
+++ spreadsheet.js
@@ -1,1 +1,16 @@
 //write your code here
+
+
+var table2 = [
+    ["Number","Name","Position","Points per Game"],
+    ["12","Joe Schmo","Center",[14,32,7,0,23]],
+    ["9","Ken Buckets","Point Guard", [19,0,11,22,0]],
+    ["31","Harvey Kay","Shooting Guard",[0,30,16,0,25]],
+    ["18","Sally Talls","Power Forward",[18,29,26,31,19]],
+    ["22","Mo DiBoux","Samll Forward",[11,0,23,17,0]]
+];
+
+console.log(table2[3][2] == "Shooting Guard");
+console.log(table2[3][0]);
+console.log(JSON.stringify(table2[1][3]) == JSON.stringify([14,32,7,0,23]));
+
