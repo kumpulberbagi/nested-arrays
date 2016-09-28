090ceab7af32690d5274a8170938cd5150afa6ef
YONI SETIAWAN <yonisetiawan1990@gmail.com>
2016-09-28 10:52:07 +0700

    chessboard done

--- chessboard.js
+++ chessboard.js
@@ -1,1 +1,16 @@
-//write your code here
+//function chessboard(){
+  var catur = [
+    ["Benteng Hitam","Kuda Hitam","Peluncur Hitam","Raja Hitam","Panglima Hitam","Peluncur Hitam","Kuda Hitam","Benteng Hitam"],
+    ["Pion Hitam","Pion Hitam","Pion Hitam","Pion Hitam","Pion Hitam","Pion Hitam","Pion Hitam","Pion Hitam"],
+    ["","","","","","","",""],
+    ["","","","","","","",""],
+    ["","","","","","","",""],
+    ["","","","","","","",""],
+    ["Pion Putih","Pion Putih","Pion Putih","Pion Putih","Pion Putih","Pion Putih","Pion Putih","Pion Putih"],
+    ["Benteng Putih","Kuda Putih","Peluncur Putih","Raja Putih","Panglima Putih","Peluncur Putih","Kuda Putih","Benteng Putih"]
+  ]
+  //if(catur[7][0]=="Benteng Putih"){
+    console.log(catur[7][0])//=="Benteng Putih"
+
+  //}
+//}
