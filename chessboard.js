5f3f6f7c40f319dd23f0313076487ef539095328
Ken Duigraha Putra <kenduigraha@yahoo.com>
2016-09-28 11:46:52 +0700

    nested array

--- chessboard.js
+++ chessboard.js
@@ -1,1 +1,61 @@
 //write your code here
+var chessboard = [['benteng hitam', 'kuda hitam', 'gajah hitam', 'raja hitam',
+				   'ratu hitam', 'gajah hitam', 'kuda hitam', 'benteng hitam'],
+				  ['pion hitam', 'pion hitam', 'pion hitam', 'pion hitam', 'pion hitam', 'pion hitam', 'pion hitam', 'pion hitam'],
+				  [0,0,0,0,0,0,0,0],
+				  [0,0,0,0,0,0,0,0],
+				  [0,0,0,0,0,0,0,0],
+				  [0,0,0,0,0,0,0,0],
+				  ['pion putih', 'pion putih', 'pion putih', 'pion putih', 'pion putih', 'pion putih', 'pion putih', 'pion putih'],
+				  ['benteng putih', 'kuda putih', 'gajah putih', 'raja putih',
+				   'ratu putih', 'gajah putih', 'kuda putih', 'benteng putih']];
+
+//console.log(chessboard[7][7]);
+
+var chessboard2 = new Array(8);
+
+for(var i = 0; i < 8 ; i++){
+	chessboard2[i] = new Array(8);
+	for(var j = 0 ; j < 8 ; j++){
+		chessboard2[i][j] = 0;
+	}
+}
+
+for(var i = 0 ; i < chessboard2.length ; i++){
+	for(var j = 0 ; j < 8 ; j++){
+		if(i == 1){
+			chessboard2[i][j] = "Pion Hitam";
+		}else if(i == 6){
+			chessboard2[i][j] = "Pion Putih";
+		}else if(i == 0){
+			if(j == 0 || j == 7){
+				chessboard2[i][j] = "Benteng Hitam";
+			}else if(j == 1 || j == 6){
+				chessboard2[i][j] = "Kuda Hitam";	
+			}else if(j == 2 || j == 5){
+				chessboard2[i][j] = "Gajah Hitam";	
+			}else if(j == 3){
+				chessboard2[i][j] = "Raja Hitam";
+			}else{
+				chessboard2[i][j] = "Ratu Hitam";	
+			}
+		}else if(i == 7){
+			if(j == 0 || j == 7){
+				chessboard2[i][j] = "Benteng Putih";
+			}else if(j == 1 || j == 6){
+				chessboard2[i][j] = "Kuda Putih";	
+			}else if(j == 2 || j == 5){
+				chessboard2[i][j] = "Gajah Putih";	
+			}else if(j == 3){
+				chessboard2[i][j] = "Raja Putih";
+			}else{
+				chessboard2[i][j] = "Ratu Putih";	
+			}
+		}
+	}
+}
+
+for(var i = 0 ; i < 8 ; i++){
+	console.log(chessboard2[i] + " ");
+	console.log("\n");
+}
--- spreadsheet.js
+++ spreadsheet.js
@@ -1,1 +1,10 @@
 //write your code here
+var tabel = [['Number', 'Name', 'Position', 'Points per Game'],
+			  [12, 'Joe Schmo', 'Center', [14, 32, 7, 0, 23]],
+			  [9, 'Ken Buckets', 'Point Guard', [19, 0, 11, 22, 0]],
+			  [31, 'Harvey Kay', 'Shooting Guard', [0, 30, 16, 0, 25]],
+			  [18, 'Sally Talls', 'Power Guard', [18, 29, 26, 31, 19]],
+			  [22, 'Mo DiBoux', 'Small Forward', [11, 0, 23, 17, 0]]];
+
+console.log(tabel[3][2] == "Shooting Guard");
+console.log(tabel[1][3][0]);
